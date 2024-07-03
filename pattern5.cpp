f4576835e2fea92d01b88e2e3f2da08142819dd5
Vishal Kurve <vishalkurve7@gmail.com>
2024-07-04 00:30:06 +0530

    Create pattern5.cpp
    solution for pattern5 problem in cpp

---
+++ pattern5.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    int row=2*n;
+    int col=(n*n)-n+3;
+    vector<vector<char>>mat(row,vector<char>(col,' '));
+    int start=1;
+    int end=n;
+    int count=0;
+    for(int i=0;i<row;i++)
+    {
+        int count=0;
+        for(int j=0;j<col;j++)
+        {
+            if(i>=0 && i<=n && j==0 || i>=n-1 && j==col-1)
+            {
+                mat[i][j]='@';
+            }
+            if(i>=n/2 && i<(n/2)+n && j>=start && j<=end && count<n)
+            {
+                mat[i][j]='*';
+                count++;
+            }
+        }
+            if(i>=n/2 && i<(n/2)+n )
+            {
+                start=end;
+                end+=n-1;
+            }
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<mat[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
