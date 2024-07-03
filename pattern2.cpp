77f6aa6b548f9fe3b7822d039245fb46dbd3df6f
Vishal Kurve <vishalkurve7@gmail.com>
2024-07-03 23:12:33 +0530

    Create pattern2.cpp
    solution for pattern 2 problem

---
+++ pattern2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter any number"<<endl;
+    cin>>n;
+    int row=n*3;
+    int col=(n*n)-n+3;
+    vector<vector<char>> mat(row ,vector<char>(col,' '));
+    int start=1;
+    int end=n;
+    for(int i=0;i<row;i++)
+    {
+        int count=0;
+        for(int j=0;j<col;j++)
+        {
+            if(i<=n && j==0 || i>= (2*n-1) && j==col-1)
+            {
+                mat[i][j]='@';
+            }
+            if(i>=n && i<2*n && j>=start && j<=end && count<n)
+            {
+                mat[i][j]='*';
+                count++;
+            }
+        }
+        if(i>=n && i<2*n){
+        start=end;
+        end+=n-1;}
+        cout<<endl;
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<mat[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
