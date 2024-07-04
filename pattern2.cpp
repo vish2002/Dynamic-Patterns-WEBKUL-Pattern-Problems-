064163fa424549528a7dbff686d172654bfc68df
Vishal Kurve <vishalkurve7@gmail.com>
2024-07-04 14:23:13 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,3 +1,22 @@
+/*
+
+Code Output
+
+Enter any number
+3
+
+@        
+@        
+@        
+@***     
+   ***   
+     ***@
+        @
+        @
+        @
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
